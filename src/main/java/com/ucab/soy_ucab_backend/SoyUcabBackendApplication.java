239eb0db77cf9f066f0978ddd39349a016110f8f
Miguel Pinto <miguelep0106@gmail.com>
2025-12-07 19:56:18 -0400

    feat: commit de vainaas

---
+++ src/main/java/com/ucab/soy_ucab_backend/SoyUcabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ucab.soy_ucab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoyUcabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoyUcabBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucab/soy_ucab_backend/SoyUcabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucab.soy_ucab_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoyUcabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
